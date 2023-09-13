fd7f55b1898f663179c6dbc2ce97336d4d684e77
Jia-Jia Zhu <135554427+zhus-on-first@users.noreply.github.com>
2023-09-13 14:28:01 -0700

    fun

--- lib/student.py
+++ lib/student.py
@@ -3,6 +3,9 @@
 from user import User
 
 class Student(User):
-    
-    def learn(self):
-        pass
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = []
+
+    def learn(self, new_knowledge):
+        self.knowledge.append(new_knowledge)
--- lib/user.py
+++ lib/user.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
 
 class User:
-    pass
+    def __init__(self, first_name, last_name) -> None:
+       self.first_name = first_name
+       self.last_name = last_name
+
+       
